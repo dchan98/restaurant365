742e1ab6bd37fa95cb10dcb78aa19afd5d8a7e3f
Danny Chan <dchan@Dannys-MacBook-Pro-2.local>
2019-10-12 13:22:09 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restaurant365
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
